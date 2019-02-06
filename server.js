0be8a7be0c2e5f28558fcea12e8d21abc61c8c33
Joy <stepjoyous@gmail.com>
2019-02-06 15:53:01 -0800

    Project 4 Capstone: first init

---
+++ server.js
@@ -0,0 +1,40 @@
+// REQUIRE dotenv file
+require('dotenv').config();
+
+// CONSTANTS TO REQUIRE FROM EXTERNAL FILES
+const
+    express = require('express'),
+    app = express(),
+    path = require('path'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    cors = require('cors'),
+    axios = require('axios'),
+    MONGODB_URI = process.env.MONGDOB_URI || 'mongodb://localhost:27017/capstone',
+    PORT = process.env.PORT || 3000;
+
+// CONNECT TO MONGOOSE
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, (err) => {
+    console.log(err || `Connected to mLab`)
+})
+
+// CONFIGURATIONS
+
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+
+// ROUTES
+
+
+
+// LISTENING PORT
+app.listen(PORT, err => {
+    console.log(err || `Server listening on PORT ${PORT}`);
+});
