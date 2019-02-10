a93a4f336572d8abcca8de6a720f23126d4356ab
Joy <stepjoyous@gmail.com>
2019-02-10 14:37:42 -0800

    Added static home page.

---
+++ client/src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => (
+    <div className="aboutBox container clearfix">
+        <div className="float-left">
+            <h1> PORT=fol10 </h1>
+            <h3>Showcase your projects here!</h3>
+            <p>Upload your screenshots, links and code to showcase your skills here.</p>
+            <h3>Filler Header</h3>
+            <p>Cupcake ipsum dolor. Sit amet cookie marzipan gummies sweet. Tart lollipop tiramisu sweet jelly halvah cookie. Gummi bears donut soufflé. Sweet roll cupcake chupa chups gummi bears lollipop. Macaroon cake cookie toffee tiramisu marzipan cookie powder. Apple pie danish icing cotton candy halvah liquorice chocolate cake marshmallow. Gummi bears oat cake sweet macaroon. Brownie pastry jujubes biscuit macaroon lemon drops powder.</p>
+        </div>
+    </div>
+    
+)
